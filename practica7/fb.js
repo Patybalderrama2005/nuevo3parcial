496b9ca931d6ed01f821ee544d967e74cc4a1210
Patybalderrama2005 <nellypatriciabalderramaalanis@gmail.com>
2022-11-22 09:52:15 -0600

    AGREGUE 3 PRACTICAS

---
+++ practica7/fb.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const fbConfig = {
+  apiKey: "AIzaSyAum9V6vgZZ1t6Ihu-5nN8cVVSSs3OZJ-4",
+  authDomain: "mipremeravezfirebase.firebaseapp.com",
+  projectId: "mipremeravezfirebase",
+  storageBucket: "mipremeravezfirebase.appspot.com",
+  messagingSenderId: "80032944318",
+  appId: "1:80032944318:web:fb8049fb963ca69a244eef",
+  measurementId: "G-MDGWKG3W5N"
+};
+
+// Initialize Firebase
+export const app = initializeApp(fbConfig);
